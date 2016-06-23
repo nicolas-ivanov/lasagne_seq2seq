a867ddd4e20079986e5a706d73db3e6322978509
Khalman Mikhail <khalman.m@gmail.com>
2016-06-23 17:17:54 +0300

    generated different words + fix generator

--- bin/generate_toy_dataset.py
+++ bin/generate_toy_dataset.py
@@ -19,9 +19,10 @@ def create_dataset(output_filename, words_list, n_lines=10000, n_words_in_line=3
     with open(output_filename, 'w') as f:
         for i in xrange(n_lines):
             if same_words:
-                words = np.random.choice(words_list, size=n_words_in_line)
-            else:
                 words = [np.random.choice(words_list)] * n_words_in_line
+            else:
+                words = np.random.choice(words_list, size=n_words_in_line)
+
             line_to_use = ' '.join(words) + '\n'
             f.write(line_to_use)
             if reversed_output:
@@ -31,5 +32,9 @@ def create_dataset(output_filename, words_list, n_lines=10000, n_words_in_line=3
 
 if __name__ == '__main__':
     words_list = get_unique_word_list('data/train/movie_lines_cleaned_10k.txt')
+    create_dataset('data/train/repeated_words.txt', words_list, same_words=True)
+    create_dataset('data/test/repeated_words.txt', words_list, same_words=True, n_lines=50)
     create_dataset('data/train/repeated_phrases.txt', words_list)
     create_dataset('data/test/repeated_phrases.txt', words_list, n_lines=50)
+    create_dataset('data/train/reversed_phrases.txt', words_list, reversed_output=True)
+    create_dataset('data/test/reversed_phrases.txt', words_list, n_lines=50, reversed_output=True)
