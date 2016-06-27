64c7c22e36fd0bb9d6b0219b701b6fecb25a7599
Khalman Mikhail <khalman.m@gmail.com>
2016-06-27 22:27:47 +0300

    add debug output for train lines

--- lib/nn_model/train.py
+++ lib/nn_model/train.py
@@ -94,11 +94,18 @@ def train_model(nn_model,tokenized_dialog_lines, validation_lines, index_to_toke
                     print '\n', datetime.datetime.now().time()
                     print NN_MODEL_PARAMS_STR, '\n'
 
+                    print 'Test dataset:'
                     for i, sent in enumerate(test_dataset):
                         for t in [0.5, 0.3, 0.1, 0.03, 0.01]:
                             prediction, perplexity = get_nn_response(x_test[i], nn_model, index_to_token, temperature=t)
                             print '%-35s\t --t=%0.3f--> \t[%.2f]\t%s' % (sent, t, perplexity, prediction)
-                    print '\n'
+                    print
+                    print 'Train dataset:'
+                    for i, sent in enumerate(all_train_lines[:SMALL_TEST_DATASET_SIZE]):
+                        for t in [0.5, 0.3, 0.1, 0.03, 0.01]:
+                            prediction, perplexity = get_nn_response(X_ids[i], nn_model, index_to_token, temperature=t)
+                            print '%-35s\t --t=%0.3f--> \t[%.2f]\t%s' % (sent, t, perplexity, prediction)
+
 
                 if batch_id % BIG_TEST_PREDICTIONS_FREQUENCY == 0:
                     plot_loss(loss_history)
