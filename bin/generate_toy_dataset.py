cb6a10b8199b3dd5fc39f517ec0352a1984274da
Khalman Mikhail <khalman.m@gmail.com>
2016-06-28 16:54:17 +0300

    add new data

--- bin/generate_toy_dataset.py
+++ bin/generate_toy_dataset.py
@@ -32,7 +32,7 @@ def create_dataset(output_filename, words_list, n_lines=1000000, n_words_in_line
                 line_to_use = ' '.join(reversed(words)) + '\n'
             if not skip_answers:
                 f.write(line_to_use)
-            if i % (n_lines / 10000) == 0:
+            if i % (n_lines / 1000) == 0:
                 sys.stdout.write('\r%0.2f%%' % (float(i) / n_lines * 100.0))
                 sys.stdout.flush()
         sys.stdout.write('\n')
@@ -40,13 +40,13 @@ def create_dataset(output_filename, words_list, n_lines=1000000, n_words_in_line
 
 if __name__ == '__main__':
     test_lines_number = 1000
-    words_list = get_unique_word_list('data/train/movie_lines_cleaned.txt')
-    create_dataset('data/train/repeated_words_1m.txt', words_list, same_words=True)
-    #create_dataset('data/test/repeated_words.txt', words_list, skip_answers=True, same_words=True,
-    #               n_lines=test_lines_number)
-    create_dataset('data/train/repeated_phrases_1m.txt', words_list)
-    #create_dataset('data/test/repeated_phrases.txt', words_list, skip_answers=True,
-    #               n_lines=test_lines_number)
-    create_dataset('data/train/reversed_phrases_1m.txt', words_list, reversed_output=True)
-    #create_dataset('data/test/reversed_phrases.txt', words_list, skip_answers=True, reversed_output=True,
-    #               n_lines=test_lines_number)
+    words_list = get_unique_word_list('data/train/repeated_phrases.txt')
+    # create_dataset('data/train/repeated_words_1m.txt', words_list, same_words=True)
+    create_dataset('data/test/repeated_words.txt', words_list, skip_answers=True, same_words=True,
+                  n_lines=test_lines_number)
+    # create_dataset('data/train/repeated_phrases_1m.txt', words_list)
+    create_dataset('data/test/repeated_phrases.txt', words_list, skip_answers=True,
+                  n_lines=test_lines_number)
+    # create_dataset('data/train/reversed_phrases_1m.txt', words_list, reversed_output=True)
+    create_dataset('data/test/reversed_phrases.txt', words_list, skip_answers=True, reversed_output=True,
+                  n_lines=test_lines_number)
